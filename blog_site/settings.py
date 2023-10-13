23ef6c5a747c75ce41ea3d213b882d5dc9122fc3
Julia Chuprova <nikaniks57@gmail.com>
2023-10-13 14:32:00 +0300

    update views and templates

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'blog_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangoblog',
+        'USER': 'blogadmin',
+        'PASSWORD': 'blogpass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
