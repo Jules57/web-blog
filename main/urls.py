ddc2a02f4b64a3bd667d0cb8b90239142957b891
Julia Chuprova <nikaniks57@gmail.com>
2023-11-21 15:52:59 +0200

    add user profile options

--- main/urls.py
+++ main/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 
 from .API.urls import router
 from .views import AboutView, ArticleDetailView, TopicListView, ArticleListView, CommentCreateView, CommentDeleteView, \
-    ArticleCreateView, ArticleUpdateView, Login, Register, Logout
-from .views import ArticleDeleteView, subscribe_on_topics, unsubscribe_from_topics, show_profile
-from .views import set_password, deactivate
+    ArticleCreateView, ArticleUpdateView, Login, Register, Logout, UserDetailView, UserUpdateView
+from .views import ArticleDeleteView, subscribe_on_topics, unsubscribe_from_topics
+from .views import UserPasswordChangeView, UserDeleteView
 
 app_name = 'main'
 
@@ -30,11 +30,12 @@ urlpatterns = [
     path('topics/<int:topic_id>/unsubscribe/',
          unsubscribe_from_topics,
          name='unsubscribe_topic'),
-    path('profile/<str:username>/',
-         show_profile,
+    path('profile/<int:pk>/',
+         UserDetailView.as_view(),
          name='show_profile'),
-    path('set-password/', set_password, name='set_password'),
-    path('deactivate/', deactivate, name='deactivate_profile'),
+    path('profile/<int:pk>/set-userdata/', UserUpdateView.as_view(), name='set_userdata'),
+    path('profile/<int:pk>/set-password/', UserPasswordChangeView.as_view(), name='set_password'),
+    path('profile/<int:pk>/deactivate/', UserDeleteView.as_view(), name='deactivate_profile'),
     path('register/', Register.as_view(), name='register'),
     path('login/', Login.as_view(), name='login'),
     path('logout/', Logout.as_view(), name='logout'),
