b9231a1e9752f998024a03cdfbfbcf39f28f7042
Julia Chuprova <nikaniks57@gmail.com>
2023-11-28 14:04:13 +0200

    update functionality of adding comments

--- main/forms.py
+++ main/forms.py
@@ -23,15 +23,16 @@ class ArticleCreateForm(forms.ModelForm):
 class CommentCreateForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['message']
+        fields = ['message', 'article']
+        widgets = {
+            "article": forms.HiddenInput()
+        }
 
-    def __init__(self, *args, **kwargs):
-        if 'request' in kwargs:
-            self.request = kwargs.pop('request')
-        if 'article_id' in kwargs:
-            self.article_id = kwargs.pop('article_id')
-        self.user = kwargs.pop('user', None)
-        super(CommentCreateForm, self).__init__(*args, **kwargs)
+    # def __init__(self, *args, **kwargs):
+    #
+    #     if 'article_id' in kwargs:
+    #         self.article_id = kwargs.pop('article_id')
+    #     super(CommentCreateForm, self).__init__(*args, **kwargs)
 
 
 class SearchForm(forms.Form):
